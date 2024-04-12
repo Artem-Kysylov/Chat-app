01d5f51d39a0e66871f76f2879b92b247096e82d
kysyl <kysylyov.artem@gmail.com>
2024-04-12 19:19:40 +0300

    update styles for message

--- chat-app/src/components/message/Message.jsx
+++ chat-app/src/components/message/Message.jsx
@@ -4,15 +4,13 @@ import React from 'react'
 export const Message = () => {
   return (
     <div className='message__wrapper owner'>
-      <div className="message__items">
-        <div className="message__info ">
+      <div className="message__info ">
           <img className='message__info-img' src="https://images.pexels.com/photos/1121796/pexels-photo-1121796.jpeg" alt="/" />
-          <div className="message__info-content">
-            <p className="message__info-content-text">Hello</p>
-            {/* <img className='message__info-content-img' src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="/" /> */}
-          </div>
-        </div>
-        <span className='message__date'>Just now</span>
+          <span className='message__date'>Just now</span>
+      </div>
+      <div className="message__info-content">
+          <p className="message__info-content-text">Hello</p>
+          {/* <img className='message__info-content-img' src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="/" /> */}
       </div>
     </div>
   )
